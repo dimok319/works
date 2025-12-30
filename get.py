c199f713b62edf5d03643bf3b9d91ebefdea056a
dimok319 <dimok319@mail.ru>
2025-12-30 20:24:41 +0300

    Update get.py

--- get.py
+++ get.py
@@ -6,19 +6,28 @@ url = "https://jsonplaceholder.typicode.com/posts"
 
 items = []
 page = 1
-limit = 20  # нормальный размер страницы
+limit = 50  # нормальный размер страницы
 
 while True:
     resp = re.get(url, params={"_page": page, "_limit": limit})
     resp.raise_for_status()
     data = resp.json()
 
+
     if not data:
         break
     print(f"Проход {page-1}")
-    items.extend(data)
+
 
     page += 1
-    ti.sleep(1)
+    ti.sleep(2)
+    items.extend(data)
 
 df = pd.DataFrame(items)
+pd.set_option("display.max_row", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+pd.set_option("display.width", None)
+pd.set_option("display.expand_frame_repr", None)
+print(df)
+
