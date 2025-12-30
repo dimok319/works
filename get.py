58f70664d249c963bbd12a82007d43270335bb37
dimok319 <dimok319@mail.ru>
2025-12-30 10:12:09 +0300

    Update get.py

--- get.py
+++ get.py
@@ -1,16 +1,23 @@
+import time
 import requests
 import pandas as pd
 
 url = "https://jsonplaceholder.typicode.com/posts"
+
 all_posts = []
+page = 1
+limit = 10
 
-# Просто делаем 10 запросов - по одному на каждую страницу
-for page_num in range(1, 11):
-    response = requests.get(url, params={'_page': page_num, '_limit': 10})
-    page_data = response.json()
-    all_posts.extend(page_data)
+while True:
+    data = requests.get(url, params={"_page": "page", "_limit": "limit"}).json()
+    if not data:
+        break
 
-# Теперь в all_posts ВСЕ 100 записей
+    all_posts += data
+    page += 1
+    print("партия прошла")
+    time.sleep(60)
+
 df = pd.DataFrame(all_posts)
-print(f"Всего записей: {len(df)}")
+print("Всего в списке:", len(df))
 print(df.head())
